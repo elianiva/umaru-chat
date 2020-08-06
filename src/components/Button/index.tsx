80f203e99fa4582775afa8b834a6e7700d7597c8
elianiva <dicha.arkana03@gmail.com>
2020-08-06 22:25:51 +0700

    added button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { FunctionalComponent, h } from "preact"
+import * as Style from "./style.css"
+
+const Button: FunctionalComponent<{
+  text: string
+  onClick?: () => void
+  inactive?: boolean
+}> = ({ text, onClick, inactive }) => {
+  return (
+    <button class={inactive ? Style.active : Style.inactive} onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+export default Button
