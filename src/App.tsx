466b35603ac7ec19e7412112b118d0cc7cada81f
elianiva <dicha.arkana03@gmail.com>
2020-08-12 19:53:00 +0700

    fix: fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import "./styles/index.css"
+import Notfound from "./pages/NotFound"
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Login />
         </Route>
         <Route path="/login">
@@ -16,6 +17,9 @@ export default function App() {
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="*">
+          <Notfound />
         </Route>
       </Switch>
     </Router>
