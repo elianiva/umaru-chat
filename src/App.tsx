b16ca63c3f641542c99417619c39cb9a52a34acb
elianiva <dicha.arkana03@gmail.com>
2020-08-12 18:08:37 +0700

    feat: basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import "./styles/index.css"
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
