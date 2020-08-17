dec536e53e61eea49294f664dc114f982b055cae
elianiva <dicha.arkana03@gmail.com>
2020-08-17 15:43:13 +0700

    feat: added client side validation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,28 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, useContext } from "react"
 import Umaru from "../../assets/umaru.png"
 import Form from "../../components/Form"
 import Button from "../../components/Button"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import { useForm } from "../../hooks/useForm"
 import "./style.css"
+import { FirebaseContext } from "../../components/Firebase"
 
 const Login: FunctionComponent = () => {
   const [formValue, setFormValue] = useForm({
     email: "",
     password: "",
   })
+  const firebase = useContext(FirebaseContext)
+  const history = useHistory()
+
+  const isInvalid = formValue.email === "" || formValue.password === ""
+
+  const login = () => {
+    const { email, password } = formValue
+    firebase.login(email, password).then(() => {
+      history.push("/rooms")
+    })
+  }
 
   return (
     <div className="login">
@@ -44,7 +56,7 @@ const Login: FunctionComponent = () => {
           }
           autocomplete="off"
         />
-        <Button text="Login" onClick={() => console.log(formValue)} />
+        <Button disabled={isInvalid} text="Login" onClick={() => login()} />
         <span className="login__message">
           Don’t have any account yet? <Link to="/register">Register here</Link>
         </span>
