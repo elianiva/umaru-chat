361d91196bc2d8a71c8150552e43f4a1852d1a21
elianiva <dicha.arkana03@gmail.com>
2020-08-21 10:08:42 +0700

    feat: integrated firebase realtime database

--- src/pages/RoomList/index.tsx
+++ src/pages/RoomList/index.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, useContext } from "react"
+import React, {
+  FunctionComponent,
+  useContext,
+  useEffect,
+  useState,
+} from "react"
 import { useHistory } from "react-router-dom"
 import { FirebaseContext, UserContext } from "../../components/Firebase"
 import Navbar from "../../components/Navbar"
@@ -8,6 +13,7 @@ import RoomCard from "../../components/RoomCard"
 
 const RoomList: FunctionComponent = () => {
   const firebase = useContext(FirebaseContext)
+  const [rooms, setRooms] = useState([])
   const history = useHistory()
   const user = useContext(UserContext)
 
@@ -23,21 +29,46 @@ const RoomList: FunctionComponent = () => {
   const defaultProfile =
     "https://firebasestorage.googleapis.com/v0/b/umaru-chat.appspot.com/o/user_profile%2Fdefault.svg?alt=media&token=28d6b78a-a42b-46f9-af4b-0fbb3dad8b4e"
 
+  const snapshotToArray = (snapshot): any => {
+    let result: any[] = []
+    snapshot.forEach((childSnapshot) => {
+      const item = childSnapshot.val()
+      item.key = childSnapshot.key
+      result.push(item)
+    })
+    return result
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      firebase.database.ref("rooms/").on("value", (resp) => {
+        setRooms(snapshotToArray(resp))
+        console.log(snapshotToArray(resp))
+      })
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
       <Navbar onClick={logout} />
       <div className="room">
         <ProfileCard name={displayName} email={email} pict={defaultProfile} />
         <h1 className="room__title">Room List</h1>
-        {[1, 2, 3, 4].map((item) => (
-          <RoomCard
-            title="anjay mabar"
-            desc="room ga jelas buat bacot njir oakwoakw "
-            url="#"
-            current={4}
-            max={10}
-          />
-        ))}
+        <hr />
+        {rooms.length < 1 ? (
+          <span className="room__notice">No rooms available</span>
+        ) : (
+          rooms.map((room) => (
+            <RoomCard
+              title="anjay mabar"
+              desc="room ga jelas buat bacot njir oakwoakw "
+              url="#"
+              current={4}
+              max={10}
+            />
+          ))
+        )}
       </div>
     </div>
   )
