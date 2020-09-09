88f6da136e3bc6fbbd3ff6b1ddf70e02a5d39609
elianiva <dicha.arkana03@gmail.com>
2020-09-09 15:41:15 +0700

    feat: added generics for useForm hooks

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,9 +1,7 @@
 import { useState } from "react"
 
-type AnyObject = { [key: string]: string }
-
-export const useForm = (initialValue: AnyObject) => {
-  const [values, setValues] = useState<AnyObject>(initialValue)
+export const useForm = <T>(initialValue: T) => {
+  const [values, setValues] = useState<T>(initialValue)
 
   const handleChange = (name: string, value: string) => {
     setValues({ ...values, [name]: value })
