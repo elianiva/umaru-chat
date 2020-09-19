89e760ecdbcde6792b49f7194cd9e1176518a6d9
elianiva <dicha.arkana03@gmail.com>
2020-09-19 12:17:26 +0700

    feat: added React.lazy for code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
 import "./styles/index.css"
-import Notfound from "./pages/NotFound"
-import RoomList from "./pages/RoomList"
-import PrivateRoute from "./components/PrivateRoute"
-import ChatRoom from "./pages/ChatRoom"
+
+// lazy loaded components
+const Login = React.lazy(() => import("./pages/Login"))
+const Register = React.lazy(() => import("./pages/Register"))
+const NotFound = React.lazy(() => import("./pages/NotFound"))
+const RoomList = React.lazy(() => import("./pages/RoomList"))
+const ChatRoom = React.lazy(() => import("./pages/ChatRoom"))
+const PrivateRoute = React.lazy(() => import("./components/PrivateRoute"))
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <PrivateRoute path="/rooms">
-          <RoomList />
-        </PrivateRoute>
-        <PrivateRoute path="/room/:id">
-          <ChatRoom />
-        </PrivateRoute>
-        <Route path="*">
-          <Notfound />
-        </Route>
-      </Switch>
+      <Suspense fallback={<div>loading</div>}>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <PrivateRoute path="/rooms">
+            <RoomList />
+          </PrivateRoute>
+          <PrivateRoute path="/room/:id">
+            <ChatRoom />
+          </PrivateRoute>
+          <Route path="*">
+            <Notfound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Router>
   )
 }
